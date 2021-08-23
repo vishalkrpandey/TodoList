66b7197b79f89cb391a5c2faa4fc8aa44f155416
Vishal Kumar Pandey <89126071+vishalkrpandey@users.noreply.github.com>
2021-08-23 04:53:39 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TodoApp from "./Components/TodoApp";
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo List</span> <br />
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
